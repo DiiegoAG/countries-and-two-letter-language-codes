4f428aa30da75fa002f3a458763f0dfe6c068c38
DiiegoAG <diegodaag07@gmail.com>
2023-12-13 16:25:30 -0600

    Method to search by country name is created

--- src/CCLL.php
+++ src/CCLL.php
@@ -6,8 +6,25 @@ define('countriesData', $countriesData);
 
 class CCLL {
 
-	public function searchByName () {
-		return 'hola';
+	/* Return an array of coincidences finded filtering by name */
+	public function searchByName ($country_name = null) {
+		if (is_null($country_name)) {
+			throw new Exception('Enter a country name to search');
+		}
+
+		$country_name = ucwords(strtolower($country_name));
+
+		$data = [];
+
+		foreach (countriesData as $country) {
+			if ($country['name'] === $country_name) {
+				array_push($data, $country);
+			}
+		}
+
+		return $data;
+	}
+
 	}
 
 }
